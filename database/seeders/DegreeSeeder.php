542706e9356698a459e2783504e3eb5571e1c75e
Tamar-Gagniashvili <tamo.gagniashvili@gmail.com>
2023-02-01 17:55:52 +0400

    fix: degree seeder

--- database/seeders/DegreeSeeder.php
+++ database/seeders/DegreeSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Degree;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\File;
 
 class DegreeSeeder extends Seeder
 {
@@ -15,11 +14,35 @@ class DegreeSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$degrees = json_decode(File::get(storage_path('json/degrees.json')));
+		$degrees = [
+			'საშუალო სკოლის დიპლომი',
+			'ზოგადსაგანმანათლებლო დიპლომი',
+			'ხელოვნების ასოცირებული ხარისხი',
+			'მეცნიერებათა ასოცირებული ხარისხი',
+			'გამოყენებითი მეცნიერების ასოცირებული ხარისხი',
+			'ხელოვნების ბაკალავრიატი',
+			'მეცნიერების ბაკალავრი',
+			'ბიზნეს ადმინისტრირების ბაკალავრი',
+			'ხელოვნების მაგისტრი',
+			'მეცნიერების მაგისტრი',
+			'ბიზნეს ადმინისტრირების მაგისტრი',
+			'იურისპრუდენციის დოქტორი',
+			'იურიდიულის ბაკალავრი',
+			'იურიდიულის მაგისტრი',
+			'მედიცინის დოქტორი',
+			'ბაკალავრი ტექნოლოგიებში',
+			'ფილოსოფიის მაგისტრი',
+			'ლიტერატურის მაგისტრი',
+			'განათლების მაგისტრი',
+			'მედიცინის ბაკალავრი',
+			'სტუდენტი',
+			'კოლეჯი(ხარისიხს გარეშე)',
+			'სხვა',
+		];
 		foreach ($degrees as $degree)
 		{
 			Degree::create([
-				'title' => $degree->title,
+				'title' => $degree,
 			]);
 		}
 	}
